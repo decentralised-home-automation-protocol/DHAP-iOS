588aff6efd30b0b56a9a83e19e21201b7e5a7e13
Aiden Garipoli <aidosgaripoli@gmail.com>
2019-09-14 14:09:09 +1000

    device view controller can now scroll its content

--- DHAP/Classes/DeviceViewController.swift
+++ DHAP/Classes/DeviceViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 open class DeviceViewController: UIViewController {
     
+    var contentView: UIView?
+    var scrollView: UIScrollView?
+    
     open override func loadView() {
         view = UIView()
         view.backgroundColor = .white
@@ -20,6 +23,36 @@ open class DeviceViewController: UIViewController {
         
         let tapGesture = UITapGestureRecognizer(target: self, action: #selector(self.dismissActiveSelection))
         self.view.addGestureRecognizer(tapGesture)
+        
+        let scrollView = UIScrollView()
+        self.scrollView = scrollView
+        scrollView.translatesAutoresizingMaskIntoConstraints = false
+        
+        self.view.addSubview(scrollView)
+        
+        NSLayoutConstraint.activate([
+            scrollView.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor),
+            scrollView.bottomAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.bottomAnchor),
+            scrollView.leadingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.leadingAnchor),
+            scrollView.trailingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.trailingAnchor)
+        ])
+        
+        let contentView = UIView()
+        self.contentView = contentView
+        contentView.translatesAutoresizingMaskIntoConstraints = false
+        scrollView.addSubview(contentView)
+        
+        let heightConstraint = contentView.heightAnchor.constraint(equalTo:scrollView.heightAnchor)
+        heightConstraint.priority = UILayoutPriority(rawValue: 250)
+        
+        NSLayoutConstraint.activate([
+            contentView.topAnchor.constraint(equalTo: scrollView.topAnchor),
+            contentView.bottomAnchor.constraint(equalTo: scrollView.bottomAnchor),
+            contentView.leadingAnchor.constraint(equalTo: scrollView.leadingAnchor),
+            contentView.trailingAnchor.constraint(equalTo: scrollView.trailingAnchor),
+            contentView.widthAnchor.constraint(equalTo: scrollView.widthAnchor),
+            heightConstraint
+        ])
         
         navigationController?.title = "Device"
         
@@ -46,13 +79,24 @@ open class DeviceViewController: UIViewController {
         stackView.spacing = 10
         stackView.isUserInteractionEnabled = true
         
-        self.view.addSubview(stackView)
+//        self.view.addSubview(stackView)
+        self.contentView?.addSubview(stackView)
         
-        let guide = self.view.layoutMarginsGuide
+//        let guide = self.view.layoutMarginsGuide
+//        NSLayoutConstraint.activate([
+//            stackView.leadingAnchor.constraint(equalTo: guide.leadingAnchor),
+//            stackView.trailingAnchor.constraint(equalTo: guide.trailingAnchor),
+//            stackView.topAnchor.constraint(equalTo: guide.topAnchor)
+//        ])
+        guard let contentView = self.contentView else { return }
+        
         NSLayoutConstraint.activate([
-            stackView.leadingAnchor.constraint(equalTo: guide.leadingAnchor),
-            stackView.trailingAnchor.constraint(equalTo: guide.trailingAnchor),
-            stackView.topAnchor.constraint(equalTo: guide.topAnchor)
+            stackView.topAnchor.constraint(equalTo: contentView.topAnchor),
+            stackView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor),
+            stackView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
+            stackView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor),
+            
+            stackView.widthAnchor.constraint(equalTo: contentView.widthAnchor)
         ])
     }
     
